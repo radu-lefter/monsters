7e28611efafa7799bbf6302b0f4c1b9df52d6d50
unknown <5leftr90@solent.ac.uk>
2020-08-02 18:15:56 +0100

    Updates

---
+++ precache-manifest.674d9037ba504c5ac0c1bcc72bfce22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262dab98d7a9790bcce323628b3309cd",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a073a6d41755836cca68",
+    "url": "/monsters/static/css/main.31af4466.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a073a6d41755836cca68",
+    "url": "/monsters/static/js/main.f06c3a0b.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
